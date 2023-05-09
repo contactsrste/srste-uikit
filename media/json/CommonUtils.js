3d4e5aa64f3e51d1dfc27a6843e31539680c5490
srste <contact.srste@gmail.com>
2023-05-09 05:45:05 +0530

    Converted JSON template data according to the contract

--- media/json/CommonUtils.js
+++ media/json/CommonUtils.js
@@ -1,19 +1,3 @@
 export const getBlockDataToCopy = (blockData, parentBlockId) => {
-	let result = {};    
-	result.elementId = (blockData.elementId || "") + getGUID().substring(0,4);
-
-	if(blockData.children) {
-		result.children = [];
-		blockData.children.forEach((blockDataChild, indx) => {
-			result.children[indx] = getBlockDataToCopy(blockDataChild, result.block_id);
-		});
-	}
-	return result;
-}
-
-export const getGUID = function() {
-	return 'xxxx-xxxx-yyyyyy-xxxx-yyyyxxyy'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
+	return blockData;
 }
