affba06a4c893f42927ab397906924144e95cb6f
srste <contact.srste@gmail.com>
2023-05-09 05:35:14 +0530

    Converted JSON template data according to the contract

--- media/json/CommonUtils.js
+++ media/json/CommonUtils.js
@@ -1,23 +1,6 @@
 export const getBlockDataToCopy = (blockData, parentBlockId) => {
 	let result = {};    
-    Object.keys(blockData).map((blockDataKey) => {
-        if(blockDataKey != "block_id" && blockDataKey != "parent_block_id" && blockDataKey != "defaults" && blockDataKey != "props_override"){
-            result[blockDataKey] = blockData[blockDataKey];
-        }
-    })
-	
 	result.elementId = (blockData.elementId || "") + getGUID().substring(0,4);
-
-	if(parentBlockId)  {
-		result.block_id = getGUID();
-		result.parent_block_id = parentBlockId;
-	}else {
-        result.block_id = getGUID();
-    }
-
-	if(blockData.id) {
-		result.id = getGUID();
-	}
 
 	if(blockData.children) {
 		result.children = [];
